8a3ca4e102d000890462bf8f3525ee4398dccc23
Patrick McDaniel <dfwjpm@outlook.com>
2024-05-02 11:20:37 -0500

    add server side action

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,4 +8,26 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+// this will run on the server, not client
+// triggered when a non get request is sent to this route
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = {
+    title: formData.get("title"),
+    content: formData.get("content"),
+  };
+
+  const aSecondWayNoteData = Object.fromEntries(formData);
+  console.log("aSecondWayNoteData.title", aSecondWayNoteData.title);
+  console.log("aSecondWayNoteData.content", aSecondWayNoteData.content);
+
+  // Add valicdation
+
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect("/notes");
 }
