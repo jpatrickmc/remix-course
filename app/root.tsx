b7dc1cbb8eff876c490261e24191339f84b62383
Patrick McDaniel <dfwjpm@outlook.com>
2024-05-01 22:45:46 -0500

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "~/styles/main.css";
+import MainNavigation from "~/components/MainNavigation";
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <header>
+          <MainNavigation />
+        </header>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export default function App() {
+  return <Outlet />;
+}
