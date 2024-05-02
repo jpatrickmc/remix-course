e8cc07d8bb8a59b7169fb4a7ecf70cbe0f0386ca
Patrick McDaniel <dfwjpm@outlook.com>
2024-05-02 11:48:49 -0500

    add note list and Form

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,11 +1,15 @@
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import NewNote from "~/components/NewNote";
 import { getStoredNotes, storeNotes } from "~/data/notes";
+import NoteList, { links as noteListLinks } from "~/components/NoteList";
+import { useLoaderData } from "@remix-run/react";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 }
@@ -30,4 +34,12 @@ export async function action({ request }) {
   const updatedNotes = existingNotes.concat(noteData);
   await storeNotes(updatedNotes);
   return redirect("/notes");
+}
+
+// this will run on the server, not client
+// triggered with a get request to this route
+export async function loader() {
+  const notes = await getStoredNotes();
+  // return json(notes);
+  return notes;
 }
